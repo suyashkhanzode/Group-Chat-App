dd6a2fed623851946fd95c41f4c7f42145659382
“suyashkhanzode” <suyashkhanzode123@gmail.com>
2024-06-02 19:33:22 +0530

    Lets make the Chat App real time

--- backend/controllers/chat.js
+++ backend/controllers/chat.js
@@ -1,30 +1,32 @@
-const Chat = require('../models/chat');
+const Chat = require("../models/chat");
+const User = require("../models/user");
 
-exports.addChat =  (req,res,next) =>{
-    const message = req.body.message;
-    const userId = req.user.id;
-    Chat.create({
-        message : message,
-        userId :userId
+exports.addChat = (req, res, next) => {
+  debugger;
+  const message = req.body.message;
+  const userId = req.user.id;
+  Chat.create({
+    message: message,
+    userId: userId,
+  })
+    .then((result) => {
+      res.status(201).json({ status: true });
     })
-    .then((result)=>{
-        res.status(201).json({status : true})
-    })
-    .catch((err)=>{
-        res.status(500).json({status : false})
-    })
-    
+    .catch((err) => {
+      res.status(500).json({ status: false });
+    });
+};
 
-}
+exports.getChats = (req, res, next) => {
+  Chat.findAll({
+    include: {
+      model: User,
 
-exports.getChats = (req,res,next) =>{
-    
-     Chat.findAll()
-     .then((result)=>[
-        res.status(200).json({chats:result})
-     ])
-     .catch((err)=>{
-        res.status(500).json({error :err})
-     })
-
-}
+      attributes: ["name"],
+    },
+  })
+    .then((result) => [res.status(200).json({ chats: result })])
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
+};
--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -1,7 +1,8 @@
-const token = window.sessionStorage.getItem('token');
+const token = window.sessionStorage.getItem("token");
 document.getElementById("button-addon2").addEventListener("click", () => {
   const message = document.getElementById("messageInput").value;
-  document.getElementById("messageInput").value = '';
+  document.getElementById("messageInput").value = "";
+  debugger;
   axios.post(
     `http://localhost:3000/chat/add-chat`,
     {
@@ -12,17 +13,29 @@ document.getElementById("button-addon2").addEventListener("click", () => {
         Authorization: token,
       },
     }
-  )
+  );
 });
 
-function getChats(){
-    axios.get(`http://localhost:3000/chat/get-chat`)
-    .then((response)=>{
-       console.log(response.data.chats)
+setInterval(() => {
+  axios
+    .get(`http://localhost:3000/chat/get-chat`)
+    .then((response) => {
+      console.log(response.data.chats)
+      const ele = (document.getElementById("chatBox").innerHTML = "");
+      response.data.chats.forEach((chat) => {
+        showChats(chat);
+      });
     })
-    .catch((err) =>{
-    
-})
+    .catch((err) => {});
+}, 1000);
+
+function showChats(chat) {
+  const ele = document.getElementById("chatBox");
+  const cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+  cardBody.textContent = `${chat.user.name}:${chat.message}`;
+
+  ele.appendChild(cardBody);
 }
 
-window.onload = getChats();
+
